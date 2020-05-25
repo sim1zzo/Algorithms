9d55b1a1c0f3faec9c232ff0519807ca5a3f59d9
Simone Izzo <izzosimone6@gmail.com>
2020-05-25 20:59:55 +0200

    add more modules

--- com/sim1zzo/Main.java
+++ com/sim1zzo/Main.java
@@ -5,6 +5,7 @@ import com.sim1zzo.algorithm_part2.Tree;
 public class Main {
 
     public static void main(String[] args) {
+
         Tree tree = new Tree();
         tree.insert(7);
         tree.insert(4);
@@ -13,18 +14,10 @@ public class Main {
         tree.insert(6);
         tree.insert(8);
         tree.insert(10);
+        tree.traverseLevelOrder();
 
-        Tree tree2 = new Tree();
-        tree2.insert(7);
-        tree2.insert(4);
-        tree2.insert(9);
-        tree2.insert(1);
-        tree2.insert(6);
-        tree2.insert(8);
-        tree2.insert(10);
 
-        var eq = tree.equals(null);
-        System.out.println(eq);
+
 
 
 
